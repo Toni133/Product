9848077554a4b47b4ad500b6f9f0d98ff33ceb49
Anton <toniq133@bk.ru>
2023-08-01 15:34:18 +0300

    first commit

--- src/test/java/ru/netology/javaqa/Product/ProductTest.java
+++ src/test/java/ru/netology/javaqa/Product/ProductTest.java
@@ -1,3 +1,41 @@
 package ru.netology.javaqa.Product;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ProductTest {
+    @Test
+    public void testRemoveWhenProductExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "телелфон", 30_000);
+        Product product2 = new Product(22, "книга", 300);
+        Product product3 = new Product(300, "машина", 30_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(22);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveWhenProductNotExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(11, "телелфон", 30_000);
+        Product product2 = new Product(22, "книга", 300);
+        Product product3 = new Product(300, "машина", 30_000_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.remove(44)
+        );
+    }
+
 }
